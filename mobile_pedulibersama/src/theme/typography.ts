7a14c8cb4015825eba7600a93221ef18dd1a3ff3
yogzism890 <yogznovachrono@gmail.com>
2026-04-20 15:57:15 +0700

    buat font untuk app

---
+++ mobile_pedulibersama/src/theme/typography.ts
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+
+export const Typography = StyleSheet.create({
+    h1: { fontSize: 28, fontWeight: '700' },
+    h2: { fontSize: 26, fontWeight: '700' },
+    h3: { fontSize: 22, fontWeight: '700' },
+    body: { fontSize: 14, fontWeight: '400' },
+    bodyMd: { fontSize: 16, fontWeight: '400' },
+    caption: { fontSize: 12, fontWeight: '400' },
+    label: { fontSize: 14, fontWeight: '600' },
+    button: { fontSize: 16, fontWeight: '700' },
+    buttonSm: { fontSize: 14, fontWeight: '600' },
+});
