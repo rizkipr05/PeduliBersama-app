2150af8673e313eff552d9225e4175a23a5646c9
yogzism890 <yogznovachrono@gmail.com>
2026-04-20 15:55:42 +0700

    buat warna buat app

---
+++ mobile_pedulibersama/src/theme/color.ts
@@ -0,0 +1,14 @@
+export const Colors = {
+    primary: '#006a37',
+    primaryContainer: '#088648',
+    surface: '#f9f9ff',
+    cardBackground: '#ffffff',
+    inputBackground: '#dce2f7',
+    textPrimary: '#141b2b',
+    textMuted: '#3e4a40',
+    border: '#bdcabd',
+    error: '#ba1a1a',
+    white: '#ffffff',
+    black: '#000000',
+    disabled: '#bdcabd',
+};
