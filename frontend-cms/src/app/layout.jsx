a0a8f40e79b42239f0835b89f197c872b90e8e22
Dani Suhandi <danisuhandi3@gmail.com>
2026-05-05 13:02:55 +0700

    feat: add RootLayout component for Admin CMS with global styles

---
+++ frontend-cms/src/app/layout.jsx
@@ -0,0 +1,13 @@
+import "./globals.css";
+
+export const metadata = {
+  title: "Admin CMS",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
