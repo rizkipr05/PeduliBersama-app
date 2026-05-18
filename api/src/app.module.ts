029b06ff53860189b05eccc145d2f44d718a330c
rizkipr05 <rizki.qq05@gmail.com>
2026-05-18 17:23:15 +0700

    feat: implement donation service and update disaster management with enhanced photo and needs handling

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -5,9 +5,16 @@ import { KategoriModule } from './kategori/kategori.module';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma.module';
 import { BencanaModule } from './bencana/bencana.module';
+import { DonasiModule } from './donasi/donasi.module';
 
 @Module({
-  imports: [KategoriModule, AuthModule, PrismaModule, BencanaModule],
+  imports: [
+    KategoriModule,
+    AuthModule,
+    PrismaModule,
+    BencanaModule,
+    DonasiModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
